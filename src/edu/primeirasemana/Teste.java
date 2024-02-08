d57be72927651f9ec926221bc9890708a1854e0b
lima2206 <vitor.lima2206@gmail.com>
2024-02-08 12:56:50 -0300

    Agora está arrumado

--- src/edu/primeirasemana/Teste.java
+++ src/edu/primeirasemana/Teste.java
@@ -2,8 +2,8 @@ package edu.primeirasemana;
 
 public class Teste{
     public static void main(String[] args) {
-        String asdf = "lala";
 
-        System.out.println(asdf);
+
+        
     }
 }
